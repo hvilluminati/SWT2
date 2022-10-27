b332986c56924cb303839d7c0af1f24fce22b914
hvilluminati <91546138+hvilluminati@users.noreply.github.com>
2022-10-28 00:39:50 +0200

    Added Display tests

---
+++ Ladeskab.Test.Unit/TestDisplay.cs
@@ -0,0 +1,33 @@
+﻿using Ladeskab.Interfaces;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab.Test.Unit
+{
+    public class TestDisplay
+    {
+        private DisplaySimulator _uut;
+        private StringWriter _output;
+
+        [SetUp]
+        public void Setup()
+        {
+            _output = new StringWriter();
+            _uut = new DisplaySimulator();
+            Console.SetOut(_output);
+        }
+
+        [TestCase("Test message")]
+        public void DisplayCorrectMessage(string msg)
+        {
+            _uut.Print("Test message");
+            Assert.That(_output.ToString(), Contains.Substring(msg));
+        }
+    }
+}
