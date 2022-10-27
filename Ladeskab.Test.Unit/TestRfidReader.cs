f53b38d928718e9d755b01f0c267ba57525098c7
Marcus Pedersen <marcusp97@live.dk>
2022-10-27 15:31:21 +0200

    RfidReaderTestStart

---
+++ Ladeskab.Test.Unit/TestRfidReader.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestRfidReader
+{
+    [TestFixture]
+    public class TestRfidReader
+    {
+        private rfidReader _uut;
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new rfidReader();
+        }
+
+        [Test]
+        public void ctor_IsRfidRead()
+        {
+            Assert.That(_uut.OnRfidRead, Is.!NULL);
+        }
+    }
+}
