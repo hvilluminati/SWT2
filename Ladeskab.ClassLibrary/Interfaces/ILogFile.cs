5be019cd36b9a15f70bea0c85a0435a78866b234
hvilluminati <91546138+hvilluminati@users.noreply.github.com>
2022-10-27 22:53:50 +0200

    Added LogFile

---
+++ Ladeskab.ClassLibrary/Interfaces/ILogFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab.Interfaces
+{
+    public interface ILogFile
+    {
+        string FileName { get; set; }
+        public void LogDoorLocked(int id);
+        public void LogDoorUnlocked(int id);
+    }
+}
