0c80955961e01cc5a5ecd7c21649fe3219d0bc8b
Marcus Pedersen <marcusp97@live.dk>
2022-10-27 12:48:42 +0200

    rifdEvent

--- Ladeskab.ClassLibrary/rfidReader.cs
+++ Ladeskab.ClassLibrary/rfidReader.cs
@@ -4,15 +4,11 @@ namespace RFIDReader
 {
     public class rfidReader : IRfidReader
     {
-        
-        private int _oldId;
-
         public event EventHandler<rfidEventArgs>? rfidEvent;
 
         public void scanId(int newId)
         {
             onRfid(new rfidEventArgs {id = newId});
-            _oldId = newId;
         }
 
         public void onRfid(rfidEventArgs e)
